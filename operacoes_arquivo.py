a8c39194727d064cd48bb44c53c01f06e29a96b1
Norgas <84186302+Norgas@users.noreply.github.com>
2021-05-27 11:33:58 -0300

    Add files via upload

---
+++ operacoes_arquivo.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(0, 'C:\\Users\\Zeni\\Documents\\Computer_Society\\Python\\RDNCalc\\operacoes')
+
+import operacoes
+
+from operacoes import *
+from cossecante import * 
+from cotangente import *
+from divisao import *
+from exponenciacao import *
+from fatorial import *
+from ln import *
+from modulo import *
+from multiplicacao import *
+from raiz import *
+from secante import *
+from seno_radianos import *
+from soma import *
+from subtracao import *
+from tangente import *
+from tangente_radianos import *
+from log import *
+from cosseno_radianos import *
+from seno import *
+from cosseno import *
+from tangente import *
